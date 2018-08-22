3f8818184b231a9a1f5bc1c64d65460d175e82d7
loganbrandt-lb <loganbrandt.lb@gmail.com>
2018-08-22 17:27:20 -0600

    Added Executable

---
+++ Assets/Satellite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : MonoBehaviour {
+
+public Rigidbody RB;
+public Vector3 thrusterForce;
+
+public float thrusterPower;
+
+	public void theStart(Vector3 launchVector){
+		thrusterForce = launchVector * thrusterPower;
+		RB = gameObject.GetComponent<Rigidbody>();
+		RB.velocity = thrusterForce;
+	}
+
+void OnTriggerEnter(Collider other){
+
+	Debug.Log("Collision Detected with: " + other.transform.name);
+	if(other.transform.name != "Earth"){
+		Destroy(other.gameObject);
+	}else{
+		Destroy(this.gameObject);
+	}
+}
+
+}
---
+++ Assets/SatelliteManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatelliteManager : MonoBehaviour {
+
+	Vector3 start;
+	Vector3 end;
+
+	Vector3 launchVector;
+
+	public Satellite SatPrefab;
+
+	Satellite Sat;
+
+	void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			start = Input.mousePosition;
+			Debug.Log("Start Position: " + start);
+		}
+
+		if(Input.GetMouseButtonUp(0)){
+			end = Input.mousePosition;
+			launchVector = (start - end) / 100;
+			Debug.Log("Calculated Launch Vector: " + launchVector);
+			AddSatellite(launchVector);
+		}
+	}
+
+	void AddSatellite(Vector3 LaunchVector){
+
+	Sat = Instantiate(SatPrefab, new Vector3(-9.8f,2,-25), Quaternion.Euler(0,0,0)) as Satellite;
+	Sat.theStart(launchVector);
+
+	//Use that vector 3 to add force to a new Satellite
+	//Instantiate that Satellite
+
+
+	}
+}
